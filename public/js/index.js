421500262f6354164164495297e06a50979fbc67
Amy F <snowstep231@gmail.com>
2025-06-22 12:56:29 -0400

    updated index.js with receivedPhotoURL variable, const imgDiv, etc. as specified in assignment instructions.  Tested by refeshing multiple times.  It works.  Hooray.

--- public/js/index.js
+++ public/js/index.js
@@ -5,34 +5,50 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-const quotes = [
-    {
-        quote: "Courage is found in unlikely places.",
-        author: "J. R. R. Tolkien",
-    },
-
-    {
-        quote: "When I became a man I put away childish things, including the fear of childishness and the desire to be very grown up.",
-        author: "C. S. Lewis",
-    },
-
-    {
-        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt",
+async function getRandomImage() {
+    const client_id = "y6kci3VGK0zJ32Hbu66NIkwu57KjRQMm7A1NC4PhJBs";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json()
+        const receivedPhotoUrl = returnedData.urls.regular;
+         const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error)
     }
-]
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+//  const quotes = [
+//     {
+//         quote: "Courage is found in unlikely places.",
+//         author: "J. R. R. Tolkien",
+//     },
+
+//     {
+//         quote: "When I became a man I put away childish things, including the fear of childishness and the desire to be very grown up.",
+//         author: "C. S. Lewis",
+//     },
+
+//     {
+//         quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+//         author: "Eric Schmidt",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000); 
